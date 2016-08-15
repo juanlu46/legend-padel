c5fd6ba1adc7181c38340b60141ca32d7190f51f
Juan Luis Montoya <juanlu46@gmail.com>
2016-08-15 17:47:54 +0200

    Añadida pagina del primer producto, a falta del estilo  y dos productos más

--- js/principal.js
+++ js/principal.js
@@ -1,3 +1,14 @@
-/**
- * Created by Juanlu on 13/07/2016.
- */
+$(document).foundation();
+$('.top-bar').on('sticky.zf.stuckto:top', function(){
+    $(this).addClass('shrink');
+}).on('sticky.zf.unstuckfrom:top', function(){
+    $(this).removeClass('shrink');
+});
+$.backstretch([
+    "img/imgBG/img_bg_1.jpg",
+    "img/imgBG/img_bg_2.jpg",
+    "img/imgBG/img_bg_3.jpg"
+], {
+    fade: 700,
+    duration: 5000
+});
