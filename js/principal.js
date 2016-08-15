b0b2da13e4d7bb380df4c3accfed09969ac61152
Celia2106 <celia.martinez.94@gmail.com>
2016-08-15 17:29:33 +0200

    Todas pag de productos acabadas, a falta del ponerlas con ajax, productos.html sera donde irán cargandose todas las paginas de productos, product1, produc2... son las paginas de productos que hize separadas, mañana todas iran en productos.html, tienda.html es la tab de la tienda.

--- js/principal.js
+++ js/principal.js
@@ -12,6 +12,13 @@ $(function() {
         cambiarColor(color);
 
     });
+    var target = null;
+    $('.img_thumb').hover(function(e){
+        target = $(this);
+        $(target[0].firstElementChild).fadeIn(200);
+    }, function(){
+        $(target[0].firstElementChild).fadeOut(200);
+    });
 });
 
 function abrirFoto(img) {
