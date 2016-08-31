69234b4c6c1c7b157313ade4fd94480d78fb88cf
Celia2106 <celia.martinez.94@gmail.com>
2016-08-31 19:33:46 +0200

    Primer producto cargandose dinamicamente

---
+++ carrito.php
@@ -0,0 +1,36 @@
+<?php
+?>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Legend Padel | Carrito</title>
+    <link rel="stylesheet" href="css/foundation.min.css">
+    <link rel="stylesheet" href="css/estilo.css"/>
+</head>
+<body>
+<div class="column row">
+    <a href="tienda.html">Continuar comprando</a>
+    <hr>
+    <div class="tabs-content" data-tabs-content="example-tabs">
+        <div class="tabs-panel is-active" id="panel1">
+            <h4></h4>
+            <div class="media-object stack-for-small">
+
+            </div>
+
+
+        </div>
+
+    </div>
+</div>
+
+<script src="js/jquery-2.2.0.min.js"></script>
+<script src="js/foundation.js"></script>
+<script src="js/productos.js"></script>
+<script src="js/principal.js"></script>
+<script>
+    $(document).foundation();
+</script>
+</body>
+</html>
