a796556bbf110ffbaf914085d70264061a427702
Juan Luis Montoya <juanlu46@gmail.com>
2016-11-04 21:11:32 +0100

    sds

--- startbootstrap-grayscale-gh-pages/js/grayscale.js
+++ startbootstrap-grayscale-gh-pages/js/grayscale.js
@@ -10,10 +10,29 @@ function ajustarNavBar() {
 $(window).scroll(ajustarNavBar);
 $(document).ready(function(){
     ajustarNavBar();
+    $(".marca-titulo").addClass("animated zoomInDown");
+    $(".es1").addClass("animated fadeInLeftBig");
+    $(".es2").css('visibility','visible').addClass("animated fadeInRightBig")
+    /*** copiar cuando se cargue quienes somos ***/
+    (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = "//connect.facebook.net/es_ES/sdk.js#xfbml=1&version=v2.7";
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+    $(".service,.service-tw").hover(function(){
+        $("#quienes-somos").stop(true).animate({'padding-top':'100px','padding-right': '0','padding-down': '170px', 'padding-left': '0'},300);
+    },function(){
+        $("#quienes-somos").stop(true).animate({'padding-top':'100px','padding-right': '0','padding-down': '0', 'padding-left': '0'},300);
+    });
+    $(".service-ig").hover(function(){
+        $("#quienes-somos").stop(true).animate({'padding-top':'100px','padding-right': '0','padding-down': '170px', 'padding-left': '0'},300);
+    },function(){
+        $("#quienes-somos").stop(true).animate({'padding-top':'100px','padding-right': '0','padding-down': '0', 'padding-left': '0'},300);
+    });
+    /*********/
 });
-
-//Scroll más suave
-
 
 // Controlador de scroll
 $(function() {
