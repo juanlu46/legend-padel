932ee24de2f1d56b62f1818b2cf3b405c4547388
Juan Luis Montoya <juanlu46@gmail.com>
2016-12-26 18:45:20 +0100

    adergj,

--- startbootstrap-grayscale-gh-pages/js/checkout.js
+++ startbootstrap-grayscale-gh-pages/js/checkout.js
@@ -4,6 +4,9 @@ $(document).ready(inicio);
 var vistoEnvio=false;
 
 function inicio(){
+    $('.btn-cerrar-login').on('click',cerrarFormLogin);
+    $(".btn-identificate").on("click",cargarFormIdent);
+    $('.btn-signin').on("click",validarLogin);
     if(sessionStorage.getItem('lgdusr')!=null) {
         $.get('../php/montarResumenPedido.php?usuario='+encodeURIComponent(sessionStorage.getItem('lgdusr')) , function (sProductos) {
             $('.m-progress').css('display', 'none');
@@ -27,6 +30,126 @@ function inicio(){
         $('.m-progress').css('display', 'none');
         $('.aviso').html('INICIE SESIÓN Y HAGA SU PEDIDO').css('display','block');
     }
+}
+function cargarFormIdent(){
+    $('.btn-cerrar-login').on('click',function(){
+        return false;
+    });
+    var inputEmail=$("#inputEmail");
+    inputEmail.val("");
+    $("#inputPassword").val("");
+    $('.ContentLogin').css('display','block');
+    $('.desenfoque').css('display','block');
+    if(sessionStorage.getItem('lgdusr')!=null){
+        inputEmail.val(sessionStorage.getItem('lgdusr'));
+    }
+    if(localStorage.getItem('lgdusr')!=null ){
+        inputEmail.val(localStorage.getItem('lgdusr'));
+    }
+
+}
+/* JS DE LOGIN */
+function validarLogin(){
+    var expEmail = new RegExp("^[A-Za-z]*[_a-z0-9-]+(\.[_A-Z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$");
+    var email=$("#inputEmail").val();
+    var mendaje=$(".mensaje");
+
+    //Verifica la existencia de una letra minuscula(?=.*[a-z])  y (?=.*[A-Z]) la de una letra en mayusculas.
+// Por ultimo la longitud la verificamos con los valores entre llaves {6,15}.
+    var expPass = new RegExp("^(?=.*[a-z])(?=.*[A-Z]).{6,15}$");
+    var password=$("#inputPassword").val();
+    if(!expEmail.test(email) && !expPass.test(password)) {
+        mendaje.addClass('alert-danger');
+        mendaje.text('Email y Contraseña Incorrectos');
+        mendaje.css('display', 'block');
+    }
+    else if(!expPass.test(password)) {
+        mendaje.addClass('alert-danger');
+        mendaje.text('Contraseña Incorrecta');
+        mendaje.css('display','block');
+    }
+    else  if(!expEmail.test(email)) {
+        mendaje.addClass('alert-danger');
+        mendaje.text('Email Incorrecto');
+        mendaje.css('display','block');
+    }
+    else{
+        var  arrayJson='{"email":"'+email+'",'+
+            '"password":"'+password+'"}';
+
+        $.ajax({
+            url: "php/usuarios.php",
+            type: 'POST',
+            data:"datos="+arrayJson,
+            dataType: 'text',
+            success: function(data) {
+                var mensajeAlert=$(".mensaje");
+                if(data=='Datos incorrectos, introduzca un usuario válido'){
+                    mensajeAlert.removeClass('alert-success');
+                    mensajeAlert.addClass('alert-danger');
+                    mensajeAlert.text(data);
+                    mensajeAlert.css('display','block');
+                }else {
+                    sessionStorage.setItem('lgdusr', email);
+                    mensajeAlert.addClass('alert-success');
+                    mensajeAlert.text(data);
+                    mensajeAlert.css('display', 'block');
+                    $('.ContentLogin').css('display','none');
+                    $('.desenfoque').css('display','none');
+                    addIconUsuarioMenu();
+                    location.reload();
+                }
+            }
+        });
+
+    }
+
+    if($('#chkRecordar').prop('checked')) {
+        localStorage.setItem('lgdusr',email);
+    }
+
+
+}
+function cerrarFormLogin(){
+    $('.ContentLogin').css('display','none');
+    $('.desenfoque').css('display','none');
+}
+/*Añadir opciones usuario ya logueado en elmenu*/
+function addIconUsuarioMenu() {
+    if(sessionStorage.getItem('lgdusr')!=null || localStorage.getItem('lgdusr')!=null){
+        if(sessionStorage.getItem('lgdusr')==null)
+            var emailUser='emailUser='+localStorage.getItem('lgdusr');
+        else
+            var emailUser='emailUser='+sessionStorage.getItem('lgdusr');
+        $('.btn-identificate').addClass('dropdown menu-usuario');
+        $('.btn-identificate').removeClass('btn-identificate');
+        $('aaIdentificate').remove();
+
+        $.get('php/devuelveCliente.php',emailUser,function(data){
+            var jsonCliente = JSON.parse(data);
+            var nombreCliente=jsonCliente.nombre;
+            $('.menu-usuario').html('<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">' +
+                '<span class="glyphicon glyphicon-user"></span> <span class="nombre_usuario">'+nombreCliente+'</span><span class="caret"></span></a>' +
+                '<ul class="dropdown-menu dropdown-cart" role="menu">' +
+                '<li><a class="text-center" href="html/panelCliente.html">Mi cuenta</a></li>' +
+                '<li class="divider"></li><li class="pedidosCliente"><a class="text-center" href="#">Mis pedidos</a></li>' +
+                '<li class="divider"></li> <li class="desconexion"><a class="text-center" href="#">Desconexión</a></li></ul>');
+            $('.desconexion').on('click',desconectarse);
+
+            $('.pedidosCliente').on('click',function(){
+                accederPedidosCliente(jsonCliente);});
+        });
+    }
+}
+
+function desconectarse(){
+    if(sessionStorage.getItem('lgdusr')!=null)
+        sessionStorage.removeItem('lgdusr');
+
+    if(localStorage.getItem('lgdusr')!=null)
+        localStorage.removeItem('lgdusr');
+
+    location.reload();
 }
 
 function manejadoresCartHead(){
---
+++ startbootstrap-grayscale-gh-pages/php/pagarPedido.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Juanlu
+ * Date: 26/12/2016
+ * Time: 18:13
+ */
