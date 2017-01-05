57ad7a7eaf6364b4f2eeef3be9b6b78ceb1bf50d
Juan Luis Montoya <juanlu46@gmail.com>
2017-01-05 20:02:19 +0100

    <ASDFGHJ

--- startbootstrap-grayscale-gh-pages/php/getDireccion.php
+++ startbootstrap-grayscale-gh-pages/php/getDireccion.php
@@ -1,9 +1,10 @@
 <?php
+require_once 'Encriptacion.php';
 header("Cache-Control: no-store, no-cache, must-revalidate");
 header("Cache-Control: post-check=0, pre-check=0", false);
 header("Pragma: no-cache");
-$sEmail=$_GET['usuario'];
 header('Content-Type: application/json');
+$sEmail=Encriptacion::desencriptar($_GET['usuario']);
 $conn=new mysqli("localhost","root","","legendpadel");
 $select=$conn->query("SELECT direccion, localidad, provincia, cp, telefono FROM usuarios WHERE email='".$sEmail."'");
 echo json_encode($select->fetch_assoc());
